76698f55aa8bd0ac96295c80e9af619418031d52
Danielmeza2599 <69852644+Danielmeza2599@users.noreply.github.com>
2025-03-04 15:18:57 -0600

    Create test_color_code.py
    It will contain the unit tests.

---
+++ test_color_code.py
@@ -0,0 +1,24 @@
+# Daniel Meza
+# Jr Embedded Software Developer
+# test_color_code.py
+
+#test_color_code.py: It will contain the unit tests.
+
+import color_code
+
+def test_number_to_pair(pair_number, expected_major_color, expected_minor_color):
+    major_color, minor_color = color_code.get_color_from_pair_number(pair_number)
+    assert(major_color == expected_major_color)
+    assert(minor_color == expected_minor_color)
+
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+    pair_number = color_code.get_pair_number_from_color(major_color, minor_color)
+    assert(pair_number == expected_pair_number)
+
+if __name__ == '__main__':
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('Done :)')
